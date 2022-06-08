8a7ea71ba9cde4aaae2d81950856a6cc76b6971a
Andrey <AndrEkat96@yandex.ru>
2022-06-08 21:01:47 +0500

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express'
 
 const app = express()
-const port = 3001
+const port = process.env.PORT || 5000
 
 app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello Incubator!';
